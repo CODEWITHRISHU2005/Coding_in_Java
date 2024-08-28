dd808161b02cbe3fc372615513c6277b8e09c68d
Rishabh Gupta <168845849+CODEWITHRISHU2005@users.noreply.github.com>
2024-08-28 22:55:32 +0530

    Add files via upload

---
+++ Insertion_Sort.java
@@ -0,0 +1,19 @@
+public class Insertion_Sort {
+    public static void main(String args[]) {
+        //Time Complexity:0(n^2)
+        int arr[] = {7, 8, 1, 3, 2};
+        //insertion sort
+        for(int i=1; i<arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+                while(j >= 0 && arr[j] > current) {
+                    //Keep swapping
+                    arr[j+1] = arr[j];
+                    j--;
+                }
+            arr[j+1] = current;
+        }
+        for(int element:arr)
+        System.out.print(element+" ");
+    }
+}
