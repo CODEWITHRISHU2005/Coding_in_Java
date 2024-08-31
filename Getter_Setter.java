cd83206232a0507545e56c0cea7733e874415cfa
CODEWITHRISHU2005 <rg2822046@gmail.com>
2024-09-01 00:12:45 +0530

    Add getters & setters

---
+++ Getter_Setter.java
@@ -0,0 +1,28 @@
+public class Getter_Setter {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        //-> Throws an error due to private access modifier 
+        // employee.Id = 12;
+        // employee.name = "Rishabh Gupta";
+        employee.setId(12);
+        employee.setName("Rishabh Gupta");
+        System.out.println("Employee ID: " + employee.getId());
+        System.out.println("Employee Name: " + employee.getName());
+    }
+}
+class Employee {
+    private int id;
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+}
