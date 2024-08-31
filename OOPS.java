ac111299074c47baa288f542599b6e8f3b34e30f
CODEWITHRISHU2005 <rg2822046@gmail.com>
2024-08-31 23:38:50 +0530

    Add OPPS Code

---
+++ OOPS.java
@@ -0,0 +1,32 @@
+public class OOPS {
+    public static void main(String[] args) {
+        System.out.println("My first custom class:");
+        System.out.println();
+        Employee rishu = new Employee(); //-> Instantiating a new Employee object
+        Employee john = new Employee(); //-> Instantiating a new Employee object
+
+        // Setting attributes for rishu
+        rishu.id = 56;
+        rishu.salary = 25;
+        rishu.name = "Rishabh Gupta";
+
+        // Setting attributes for john
+        john.id = 12;
+        john.name = "John";
+        john.salary = 20;
+
+        // Calling method to print details
+        rishu.printDetails(); 
+        john.printDetails(); 
+    }
+}
+class Employee{
+    int id;
+    int salary;
+    String name;
+    public void printDetails(){
+        System.out.println("Employee ID: " + id);
+        System.out.println("Employee name: " + name);
+        System.out.println("Employee salary: " + salary);
+    }
+}
