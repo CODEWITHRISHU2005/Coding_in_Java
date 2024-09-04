1f719ff48c3fa39aa25079fd15b580881e18b503
CODEWITHRISHU2005 <rg2822046@gmail.com>
2024-09-05 00:21:05 +0530

    Add my today code

---
+++ Interface.java
@@ -0,0 +1,40 @@
+// Interface can have abstract method & from java_8 can have default and static method.
+// Those class which implements all methods of interface there implemented methods must be declare as public.
+// Variables are implicitly static and final (constants).
+// Object cannot be created for interface.
+// Multiple inheritance can only be achieve through interfaces.
+
+interface Bicycle{
+    void applyBrake(int decrement);
+    void speedUP(int increment);
+}
+interface AdvBicycle{
+    void applyDisBrake();
+    void nitrospeedUP();
+}
+class AvonCycle implements Bicycle,AdvBicycle{
+    void blowHorn() {
+        System.out.println("Pee pee... po po...");
+    }
+    public void applyBrake(int decrement){
+        System.out.println("Applying Brake..");
+    }
+    public void speedUP(int increment){
+        System.out.println("Speed up...");
+    }
+    public void applyDisBrake(){
+        System.out.println("Be alert...");
+    }
+    public void nitrospeedUP() {
+        System.out.println("Buy-Buy everyone...");
+    }
+}
+public class Interface {
+    public static void main(String[] args) {
+        AvonCycle obj = new AvonCycle();
+        obj.applyBrake(1);
+        obj.speedUP(5);
+        obj.applyDisBrake();
+        obj.nitrospeedUP();
+    }
+}
