7fcca34a9165f6443c4972619c615a0026a543e1
CODEWITHRISHU2005 <rg2822046@gmail.com>
2024-09-09 00:34:27 +0530

    Add new code

--- pattern.java
+++ pattern.java
@@ -288,32 +288,29 @@ public class Pattern {
         //  ***
         // *****
         // Upper Half
-        // for(int i=3; i>=1; i--){
-        //     // spaces
-        //     for(int j=1; j<=3-i; j++){
-        //         System.out.print(" ");
-        //     }
-        //     // stars
-        //     for(int j=1; j<=2*i-1; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+//         for(int i=3; i>=1; i--){
+//             // spaces
+//             for(int j=1; j<=3-i; j++){
+//                 System.out.print(" ");
+//             }
+//             // stars
+//             for(int j=1; j<=2*i-1; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
 
         // Lower Half
-        // for(int i=1; i<=3; i++){
-        //     // spaces
-        //     for(int j=1; j<=3-i; j++){
-        //         System.out.print(" ");
-        //     }
-        //     // stars
-        //     for(int j=1; j<=2*i-1; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
-
-        // Question_16-> Write a program to print the following pattern
-        
+//         for(int i=1; i<=3; i++){
+//             // spaces
+//             for(int j=1; j<=3-i; j++){
+//                 System.out.print(" ");
+//             }
+//             // stars
+//             for(int j=1; j<=2*i-1; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
     }
 }
