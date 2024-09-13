852b3c3f3cc2b1d87798641f41e777e4e7f896c3
CODEWITHRISHU2005 <rg2822046@gmail.com>
2024-09-13 20:53:22 +0530

    add & modify my vode

--- LambdaExp.java
+++ LambdaExp.java
@@ -1,9 +1,11 @@
 // Lambda Expression only works with Functional Interface just to avoid confussion.
 
-//@FunctionalInterface
-// interface Project {
-//     void show(String name);
-// }
+@FunctionalInterface
+interface Project {
+    void show(String name);
+}
+
+@FunctionalInterface
 interface Project {
     int add(int i, int j);
 }
@@ -11,8 +13,8 @@ interface Project {
 public class LambdaExp {
     public static void main(String[] args) {
         // This is called Lambda Expression in which it is internally create obj of A.
-        // Project obj = (name) -> System.out.println("My project is:" + name);
-        // obj.show("CodeWithRishu");
+        Project obj = (name) -> System.out.println("My project is:" + name);
+        obj.show("CodeWithRishu");
 
         Project obj = (i, j) -> i + j;
         System.out.println("The Sum of two Number is: " + obj.add(7, 8));
