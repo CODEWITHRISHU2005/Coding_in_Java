7624a60cca16fe42d72e07b27fe642f3c5766dcf
CODEWITHRISHU2005 <rg2822046@gmail.com>
2025-01-31 13:52:37 +0530

    add some code

--- MyStack.java
+++ MyStack.java
@@ -1,40 +1,52 @@
 public class MyStack {
-    private static int arr[] = new int[5];
-    int top, size;
+    protected int arr[];
+    protected int top;
+    public static final int DEFAULT_CAPACITY = 5;
 
-    public MyStack() {
-        size = arr.length;
+    public MyStack() throws Exception {
+        arr = new int[DEFAULT_CAPACITY];
         top = -1;
     }
 
-    public void push(int data) {
-        if (top < size)
-            arr[++top] = data;
-        else System.out.println("Stack Overflow");
+    public MyStack(int capacity) throws Exception {
+        if (capacity < 1) {
+            throw new Exception("Invalid Stack Capacity");
+        }
+        arr = new int[capacity];
+        top = -1;
     }
 
-    public int pop() {
-        if (top > -1)
-            return arr[top--];
-        else {
-            System.out.println("Stack Underflow");
+    public void push(int data) throws Exception {
+        if (size() == arr.length) {
+            throw new Exception("Stack Overflow");
         }
-        return -1;
+        arr[++top] = data;
+    }
+
+    public int pop() throws Exception {
+        if (size() == 0) {
+            throw new Exception("Stack Underflow");
+        }
+        return arr[top--];
     }
 
     public int peek() {
         return arr[top];
     }
 
+    public int size() {
+        return top + 1;
+    }
+
     public void printStack() {
-        for (int n : arr) {
-            System.out.print(n + " ");
+        for (int i = 0; i <= top; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        MyStack nums = new MyStack();
+    public static void main(String[] args) throws Exception {
+        MyStack nums = new DynamicStack();
         nums.push(10);
         nums.push(20);
         System.out.println(nums.pop());
@@ -42,6 +54,35 @@ public class MyStack {
         nums.push(40);
         System.out.println(nums.peek());
         nums.push(50);
+        nums.push(60);
+        nums.push(70);
         nums.printStack();
+    }
+}
+
+class DynamicStack extends MyStack {
+    //Array_List or Linked_List
+    public DynamicStack() throws Exception {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public DynamicStack(int capacity) throws Exception {
+        super(capacity);
+    }
+
+    @Override
+    public void push(int data) {
+        if (size() == this.arr.length) {
+            int temp[] = new int[2 * this.arr.length];
+            for (int i = 0; i < this.arr.length; i++) {
+                temp[i] = this.arr[i];
+            }
+            this.arr = temp;
+        }
+        try {
+            super.push(data);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
